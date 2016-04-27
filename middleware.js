c3783c4415f7b7da4221045f3fa1cc74bbcd28d6
Mads Peter Jacobsen <madsissexy@gmail.com>
2016-04-27 10:09:19 +0200

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+module.exports = {
+	// middleware awesome!
+	// can be called on app route
+	// can be called on specific route
+	requireAuth: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
--- server.js
+++ server.js
@@ -2,19 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	// middleware awesome!
-	// can be called on app route
-	// can be called on specific route
-	requireAuth: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
