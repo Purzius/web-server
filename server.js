5c6fbf82d9dd2a0c581faa1cdeaf68ca8deb2632
Mads Peter Jacobsen <madsissexy@gmail.com>
2016-04-27 09:58:19 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	// middleware awesome!
+	// can be called on app route
+	// can be called on specific route
+	requireAuth: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuth ,function(req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () {
+	console.log('Express server listening on port ' + PORT + ':');
+});
